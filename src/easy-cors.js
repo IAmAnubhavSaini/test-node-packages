37a160fc01cb14ab97991e38f91f2abad5951257
Anubhav Saini <iamanubhavsaini+git@gmail.com>
2025-04-19 14:43:12 +0530

    update

--- src/easy-cors.js
+++ src/easy-cors.js
@@ -1,29 +1,3 @@
-const {middleware, easyCors} = require('@f0c1s/easy-cors')
-const red = require('@f0c1s/color-red')
-const yellow = require('@f0c1s/color-yellow')
-console.log(red(middleware.toString()), yellow(easyCors.toString()))
-
-/*
-middleware: function middleware(req, res, next) {
-    assert.equal(typeof next, 'function', 'next should be a function')
-    assert.ok(res.send, 'Response should have a send function')
-    assert.ok(res.header, 'Response should have a header function')
-    assert.ok(req.headers, 'Request should have a headers object')
-    assert.notEqual(req.headers.origin, undefined, 'Request should have a headers.origin value')
-    res.header("Access-Control-Allow-Credentials", true)
-    res.header("Access-Control-Allow-Origin", req.headers.origin)
-    res.header("Access-Control-Allow-Headers", (req.headers['access-control-request-headers']) || "Content-Type,X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5,  Date, X-Api-Version, X-File-Name")
-    res.header("Access-Control-Allow-Methods", (req.headers['access-control-request-method']) || "POST, GET, PUT, DELETE, OPTIONS")
-    if ('OPTIONS' == req.method) {
-        res.send(200)
-    }
-    else {
-        next()
-    }
-}
-easyCors: function easyCors(app) {
-    assert.ok(app.all, 'app should have all function')
-    app.all('*', middleware)
-}
-
- */
+const { easyCors } = require("@f0c1s/easy-cors");
+const red = require("@f0c1s/color-red");
+console.log(red(easyCors.toString()));
--- src/intercalate-string.js
+++ src/intercalate-string.js
@@ -1,12 +1,6 @@
-const lib = require('@f0c1s/intercalate-string')
-const red = require('@f0c1s/color-red')
+const lib = require("@f0c1s/intercalate-string");
+const red = require("@f0c1s/color-red");
 
-console.log(red(lib))
+console.log(red(lib));
 
-/* This just puts 'str:withit' in array:strings, as: a withit b withit c... and then joins them to form a single string.
-function intercalate(withit, strings) {
-    assert.ok(withit && typeof withit === 'string')
-    assert.ok(strings && Array.isArray(strings))
-    return i(withit, strings).join('')
-}
- */
+console.log(lib("\n", ["what is this?", "string b"]));
